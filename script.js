0b7dba89b8a40af942e9fd538190af2f532e4f4b
klimu69420 <gdam385@gmail.com>
2025-09-18 19:39:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openLogin() {
+  document.getElementById("loginModal").style.display = "block";
+}
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
+}
+
+function openRegister() {
+  document.getElementById("registerModal").style.display = "block";
+}
+function closeRegister() {
+  document.getElementById("registerModal").style.display = "none";
+}
